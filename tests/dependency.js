1b875694f1aafa49f0e6abaaf402170788c143b5
Michele Rastelli <michele.rastelli@jamgling.com>
2015-06-03 16:34:32 +0200

    test DI in npm dependencies

---
+++ tests/dependency.js
@@ -0,0 +1,22 @@
+delete require.cache[require.resolve('electrolyte')];
+var IoC = require('electrolyte');
+
+var expect = require('chai').expect;
+
+var myInj = {myInjectedDep: "myInjectedDep"};
+
+before(function () {
+	IoC.register("NestedDependency", myInj, "@literal");
+	IoC.register("test-dep", require("electrolyte-test-dep"));
+});
+
+describe('injectIntoDependency', function () {
+	it('instancesEquality', function () {
+		expect(IoC.create("test-dep").injected).to.eql(myInj);
+	});
+	it('instancesIdentity', function () {
+		expect(IoC.create("test-dep").injected).to.equal(myInj);
+	});
+});
+
+

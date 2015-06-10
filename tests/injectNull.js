c1b65c1de48053aef274c52189ce3185d2229f89
Michele Rastelli <michele.rastelli@jamgling.com>
2015-06-10 10:38:33 +0200

    null injection

---
+++ tests/injectNull.js
@@ -0,0 +1,24 @@
+delete require.cache[require.resolve('electrolyte')];
+var IoC = require('electrolyte');
+
+var expect = require('chai').expect;
+
+before(function () {
+	// A --> B --> C
+	// trying to inject null in place of C
+	IoC.factory("depA", require("./injectables/annotated/nestedDeps/depA"));
+	IoC.factory("depB", require("./injectables/annotated/nestedDeps/depB"));
+	IoC.factory("depC", require("./injectables/annotated/nestedDeps/depC"));
+	IoC.literal("depA/depB/depC", null);
+
+});
+
+describe('injectNull', function () {
+	it('instancesEquality', function () {
+		expect(IoC.create("depA").B.C).to.eql(null);
+		expect(IoC.create("depB").C).to.eql(IoC.create("depC"));
+		expect(IoC.create("depC")).to.eql(require("./injectables/annotated/nestedDeps/depC")());
+	});
+});
+
+
---
+++ tests/nestedDeps2.js
@@ -0,0 +1,24 @@
+delete require.cache[require.resolve('electrolyte')];
+var IoC = require('electrolyte');
+
+var expect = require('chai').expect;
+
+before(function () {
+	// A --> B --> C
+	// trying to inject X in place of C
+	IoC.factory("depA", require("./injectables/annotated/nestedDeps/depA"));
+	IoC.factory("depB", require("./injectables/annotated/nestedDeps/depB"));
+	IoC.factory("depC", require("./injectables/annotated/nestedDeps/depC"));
+	IoC.factory("depB/depC", require("./injectables/annotated/nestedDeps/depX"));
+
+});
+
+describe('nestedDeps2', function () {
+	it('instancesEquality', function () {
+		expect(IoC.create("depA").B.C).to.eql(IoC.create("depB/depC"));
+		expect(IoC.create("depB").C).to.eql(IoC.create("depB/depC"));
+		expect(IoC.create("depC")).to.eql(require("./injectables/annotated/nestedDeps/depC")());
+	});
+});
+
+

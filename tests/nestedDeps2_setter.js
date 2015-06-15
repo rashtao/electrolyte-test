9038b1fa24d4f7680a8c4cdc01cfe92ea7aca2ed
Michele Rastelli <michele.rastelli@jamgling.com>
2015-06-15 11:35:55 +0200

    nested injection with setter

---
+++ tests/nestedDeps2_setter.js
@@ -0,0 +1,24 @@
+delete require.cache[require.resolve('electrolyte')];
+var IoC = require('electrolyte');
+
+var expect = require('chai').expect;
+
+before(function () {
+	// A --> B --> C
+	// trying to inject X in place of C
+	IoC.factory("depA", require("./injectables/setter_annotated/nestedDeps/depA"));
+	IoC.factory("depB", require("./injectables/setter_annotated/nestedDeps/depB"));
+	IoC.factory("depC", require("./injectables/setter_annotated/nestedDeps/depC"));
+	IoC.factory("depB.depC", require("./injectables/setter_annotated/nestedDeps/depX"));
+
+});
+
+describe('nestedDeps2_setter', function () {
+	it('instancesEquality', function () {
+		expect(IoC.create("depA").depB.depC).to.eql(IoC.create("depB.depC"));
+		expect(IoC.create("depB").depC).to.eql(IoC.create("depB.depC"));
+		expect(IoC.create("depC")).to.eql(require("./injectables/setter_annotated/nestedDeps/depC")());
+	});
+});
+
+
